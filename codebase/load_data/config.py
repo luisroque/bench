6e07627e27ac0623b51a021378762fa048c83c90
Luis Roque <luis_roque@live.com>
2024-08-10 18:47:31 +0100

    adding wiki2 and traffic datasets

--- codebase/load_data/config.py
+++ codebase/load_data/config.py
@@ -3,8 +3,8 @@ from codebase.load_data.m3 import M3Dataset
 from codebase.load_data.tourism import TourismDataset
 from codebase.load_data.m5 import M5Dataset
 from codebase.load_data.labour import LabourDataset
-# from codebase.load_data.traffic import TrafficDataset
-# from codebase.load_data.wiki2 import Wiki2Dataset
+from codebase.load_data.traffic import TrafficDataset
+from codebase.load_data.wiki2 import Wiki2Dataset
 
 DATASETS = {
     "Tourism": TourismDataset,
@@ -12,6 +12,6 @@ DATASETS = {
     "M4": M4Dataset,
     "M5": M5Dataset,
     "Labour": LabourDataset,
-    # "Traffic": TrafficDataset,
-    # "Wiki2": Wiki2Dataset
+    "Traffic": TrafficDataset,
+    "Wiki2": Wiki2Dataset
 }
